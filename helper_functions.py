553e2f084836179348e2cdda5b63667c60f9fd67
Michał Ostrówka <m.ostrowka17@gmail.com>
2024-03-14 15:11:14 +0100

    complete

--- helper_functions.py
+++ helper_functions.py
@@ -57,18 +57,25 @@ def plot_history(history):
     plt.show()
 
 
-# Funkcja do generowania przykładowych predykcji i ich wyświetlania
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def display_test_predictions(model, dataset, class_names):
     plt.figure(figsize=(15, 15))
     for images, labels in dataset.take(1):  # Bierzemy jeden batch z datasetu
         predictions = model.predict(images)
         predictions_classes = np.argmax(predictions, axis=1)
         labels = np.argmax(labels, axis=1)  # Prawdziwe klasy
-        for i in range(25):  # Wyświetlamy pierwsze 25 obrazów z tego batcha
+
+        # Losowanie 25 unikalnych indeksów z dostępnego zakresu
+        random_indices = np.random.choice(images.shape[0], 25, replace=False)
+
+        for i, idx in enumerate(random_indices):  # Używamy losowych indeksów
             plt.subplot(5, 5, i + 1)
             plt.xticks([])
             plt.yticks([])
             plt.grid(False)
-            plt.imshow(images[i], cmap=plt.cm.binary)
-            plt.xlabel(f"Actual: {class_names[labels[i]]}\nPredicted: {class_names[predictions_classes[i]]}")
+            plt.imshow(images[idx], cmap=plt.cm.binary)
+            plt.xlabel(f"Actual: {class_names[labels[idx]]}\nPredicted: {class_names[predictions_classes[idx]]}")
     plt.show()
